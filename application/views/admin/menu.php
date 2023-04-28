119542b830fc77e0c1b3347cc9412de78a18e09c
Pirro Zani <pirrozani@outlook.com>
2023-04-28 19:49:17 +0300

    another

--- application/views/admin/menu.php
+++ application/views/admin/menu.php
@@ -1,4 +1,4 @@
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
         <div class="container-fluid">
             <span class="navbar-brand mb-0 h1">Trytodo</span>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
