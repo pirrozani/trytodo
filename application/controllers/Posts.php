19a99cc63341fe12bc4d41cea2999ceb94696d40
pirrozani <pirrozani@outlook.com>
2024-03-04 23:53:00 +0200

    refactor: Update code readability for controllers

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -1,41 +1,44 @@
 <?php
-	class Posts extends CI_Controller{
 
-		public function create(){
-			if(!$this->session->userdata('user_logged_in')){
-				redirect('users/login');
-			}
-			
-			$data['title'] = 'Submit Message';
-			$this->form_validation->set_rules('message', 'Submit Message', 'required');
+class Posts extends CI_Controller
+{
 
-			if ($this->form_validation->run() === false){
-				$this->load->view('templates/header');
-				$this->load->view('users/menu');
-				$this->load->view('posts/create', $data);
-				$this->load->view('templates/footer');
-			}else{
-				$user_id = $this->session->userdata('id');
-				$message = $this->input->post('message');
-				$this->post_model->create_post($message, $user_id);
-				$this->post_model->last_post_created($user_id);
-				$this->session->set_flashdata('message_created',
-				'Your message has been submitted!');
-				redirect('posts/create');
-				}
-			}
+	public function create()
+	{
+		if (!$this->session->userdata('user_logged_in')) {
+			redirect('users/login');
+		}
 
-		public function index(){	
-			if(!$this->session->userdata('user_logged_in')){
-				redirect('users/login');
-			}
-			$user_id = $this->session->userdata('id');
-			$data['posts'] = $this->post_model->get_posts($user_id);
+		$data['title'] = 'Submit Message';
+		$this->form_validation->set_rules('message', 'Submit Message', 'required');
+
+		if ($this->form_validation->run() === false) {
 			$this->load->view('templates/header');
 			$this->load->view('users/menu');
-			$this->load->view('posts/index', $data);
+			$this->load->view('posts/create', $data);
 			$this->load->view('templates/footer');
+		} else {
+			$user_id = $this->session->userdata('id');
+			$message = $this->input->post('message');
+			$this->post_model->create_post($message, $user_id);
+			$this->post_model->last_post_created($user_id);
+			$this->session->set_flashdata('message_created',
+				'Your message has been submitted!');
+			redirect('posts/create');
 		}
 	}
 
-  
+	public function index()
+	{
+		if (!$this->session->userdata('user_logged_in')) {
+			redirect('users/login');
+		}
+		$user_id = $this->session->userdata('id');
+		$data['posts'] = $this->post_model->get_posts($user_id);
+		$this->load->view('templates/header');
+		$this->load->view('users/menu');
+		$this->load->view('posts/index', $data);
+		$this->load->view('templates/footer');
+	}
+}
+
--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,131 +1,139 @@
 <?php
-	class Users extends CI_Controller{
 
-		public function register(){
-			$data['title'] = 'Register';
-			$this->form_validation->set_rules('firstname', 'First name', 'required');
-			$this->form_validation->set_rules('lastname', 'Last Name', 'required');
-			$this->form_validation->set_rules('email', 'Email', 'required');
-			$this->form_validation->set_rules('password', 'Password', 'required');
-			$this->form_validation->set_rules('retypepass', 'Retype Password', 'required|matches[password]');
+class Users extends CI_Controller
+{
 
-			if($this->form_validation->run()=== FALSE){
-				$this->load->view('templates/header');
-				$this->load->view('pages/home_menu');
-				$this->load->view('users/register', $data);
-				$this->load->view('templates/footer');
-			}else{
-				$data_arr = array(
-					'firstname' => $this->input->post('firstname'),
-					'lastname' => $this->input->post('lastname'),
-					'email' => $this->input->post('email'),
-					'password' => md5($this->input->post('password')),
-					'is_admin' => 0
+	public function register()
+	{
+		$data['title'] = 'Register';
+		$this->form_validation->set_rules('firstname', 'First name', 'required');
+		$this->form_validation->set_rules('lastname', 'Last Name', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('retypepass', 'Retype Password', 'required|matches[password]');
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->load->view('templates/header');
+			$this->load->view('pages/home_menu');
+			$this->load->view('users/register', $data);
+			$this->load->view('templates/footer');
+		} else {
+			$data_arr = array(
+				'firstname' => $this->input->post('firstname'),
+				'lastname' => $this->input->post('lastname'),
+				'email' => $this->input->post('email'),
+				'password' => md5($this->input->post('password')),
+				'is_admin' => 0
+			);
+			$this->user_model->register($data_arr);
+			$this->session->set_flashdata('user_registered',
+				'Your registration has been succesfully created!');
+			redirect(base_url());
+		}
+	}
+
+	public function login()
+	{
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->load->view('templates/header');
+			$this->load->view('pages/home_menu');
+			$this->load->view('pages/home');
+			$this->load->view('templates/footer');
+		} else {
+			$email = $this->input->post('email');
+			$password = md5($this->input->post('password'));
+			$user_id = $this->user_model->login($email, $password);
+
+			if ($user_id) {
+				$user_data = array(
+					'id' => $user_id,
+					'email' => $email,
+					'user_logged_in' => true
 				);
-				$this->user_model->register($data_arr);
-				$this->session->set_flashdata('user_registered',
-						'Your registration has been succesfully created!');
-				redirect(base_url());
-			}
-		}
-
-		public function login(){
-			$this->form_validation->set_rules('email', 'Email', 'required');
-			$this->form_validation->set_rules('password', 'Password', 'required');
-
-			if($this->form_validation->run() === FALSE){
-				$this->load->view('templates/header');
-				$this->load->view('pages/home_menu');
-				$this->load->view('pages/home');
-				$this->load->view('templates/footer');
+				$this->session->set_userdata($user_data);
+				$this->session->set_flashdata('user_loggedin', 'You have been succesfully logged in!');
+				redirect('users/update');
 			} else {
-				$email = $this->input->post('email');
-				$password = md5($this->input->post('password'));
-				$user_id = $this->user_model->login($email, $password);
-
-				if($user_id){
-					$user_data = array(
-						'id' => $user_id,
-						'email' => $email,
-						'user_logged_in' => true
-					);
-					$this->session->set_userdata($user_data);
-					$this->session->set_flashdata('user_loggedin', 'You have been succesfully logged in!');
-					redirect('users/update');
-				}else{
-					$this->session->set_flashdata('login_failed', 'You have typed incorrectly Email or Password.');
-					redirect('users/login');
-				}
-			}
-		}
-
-		public function logout(){
-			$array_data = array('id', 'email','user_logged_in');
-			$this->session->unset_userdata($array_data);
-			$this->session->set_flashdata('user_loggedout', 'Your have been succesfully logout!');
-			redirect('users/login');
-		}
-
-		public function update(){
-			if(!$this->session->userdata('user_logged_in')){
+				$this->session->set_flashdata('login_failed', 'You have typed incorrectly Email or Password.');
 				redirect('users/login');
 			}
-			$data['title'] = 'Update your information';
-			$this->form_validation->set_rules('firstname', 'First Name', 'required');
-			$this->form_validation->set_rules('lastname', 'Last Name', 'required');
-			$this->form_validation->set_rules('email', 'Email', 'required');
-			$this->form_validation->set_rules('password', 'Password', 'required');
-
-			if($this->form_validation->run()=== FALSE){
-				$this->load->view('templates/header');
-				$this->load->view('users/menu');
-				$this->load->view('users/update',$data);
-				$this->load->view('templates/footer');
-			}else{
-				$user_id = $this->session->userdata('id');
-				$data_arr = array(
-					'firstname' =>$this->input->post('firstname'),
-					'lastname' => $this->input->post('lastname'),
-					'email' => $this->input->post('email'),
-					'password' => md5($this->input->post('password')),
-					'is_admin' => 0
-				  );
-				$this->user_model->update($data_arr, $user_id);
-				$this->session->set_flashdata('user_updated',
-					'You have succesfully updated your information!');
-				redirect('users/update');
-
-			}
 		}
+	}
 
-		public function check_email(){
-			$this->form_validation->set_rules('email', 'Email', 'required');
+	public function logout()
+	{
+		$array_data = array('id', 'email', 'user_logged_in');
+		$this->session->unset_userdata($array_data);
+		$this->session->set_flashdata('user_loggedout', 'Your have been succesfully logout!');
+		redirect('users/login');
+	}
 
-			if($this->form_validation->run() === FALSE){
-				$this->load->view('templates/header');
-				$this->load->view('pages/home_menu');
-				$this->load->view('users/lostpass');
-				$this->load->view('templates/footer');
-
-			} else {
-				$email = $this->input->post('email');
-				$check = $this->user_model->check_email($email);
-				if(!$check){
-					$this->session->set_flashdata('wrong_email', 'Email was not found.');
-					redirect('users/check_email');
-				}
-				$this->session->set_userdata('email',$email);
-				redirect('users/send_verification_code');
-			}
+	public function update()
+	{
+		if (!$this->session->userdata('user_logged_in')) {
+			redirect('users/login');
 		}
+		$data['title'] = 'Update your information';
+		$this->form_validation->set_rules('firstname', 'First Name', 'required');
+		$this->form_validation->set_rules('lastname', 'Last Name', 'required');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
 
-		public function send_verification_code(){
+		if ($this->form_validation->run() === FALSE) {
+			$this->load->view('templates/header');
+			$this->load->view('users/menu');
+			$this->load->view('users/update', $data);
+			$this->load->view('templates/footer');
+		} else {
+			$user_id = $this->session->userdata('id');
+			$data_arr = array(
+				'firstname' => $this->input->post('firstname'),
+				'lastname' => $this->input->post('lastname'),
+				'email' => $this->input->post('email'),
+				'password' => md5($this->input->post('password')),
+				'is_admin' => 0
+			);
+			$this->user_model->update($data_arr, $user_id);
+			$this->session->set_flashdata('user_updated',
+				'You have succesfully updated your information!');
+			redirect('users/update');
 
-			$verification_code = rand(1000,9999);
-			$this->session->set_userdata('verification_code', $verification_code);
-			$to = $this->session->userdata('email');
-			$subject = 'Confirm your email';
-			$message = '<!DOCTYPE html>
+		}
+	}
+
+	public function check_email()
+	{
+		$this->form_validation->set_rules('email', 'Email', 'required');
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->load->view('templates/header');
+			$this->load->view('pages/home_menu');
+			$this->load->view('users/lostpass');
+			$this->load->view('templates/footer');
+
+		} else {
+			$email = $this->input->post('email');
+			$check = $this->user_model->check_email($email);
+			if (!$check) {
+				$this->session->set_flashdata('wrong_email', 'Email was not found.');
+				redirect('users/check_email');
+			}
+			$this->session->set_userdata('email', $email);
+			redirect('users/send_verification_code');
+		}
+	}
+
+	public function send_verification_code()
+	{
+
+		$verification_code = rand(1000, 9999);
+		$this->session->set_userdata('verification_code', $verification_code);
+		$to = $this->session->userdata('email');
+		$subject = 'Confirm your email';
+		$message = '<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
@@ -151,7 +159,7 @@
 				<hr>
 				<h3 class="text-teal-700">Insert the code in the application that is shown below:</h3>
 					<div class="code">
-						<span>'.$verification_code.'</span>
+						<span>' . $verification_code . '</span>
 					</div>
 					<h4>Thank you for using our website!</h4>
 			 <hr>
@@ -159,61 +167,63 @@
 			</body>
 			</html>';
 
-			$this->email->set_newline("\r\n");
-			$this->email->from('smtp_user', 'Trytodo');
-			$this->email->to($to);
-			$this->email->subject($subject);
-			$this->email->message($message);
+		$this->email->set_newline("\r\n");
+		$this->email->from('smtp_user', 'Trytodo');
+		$this->email->to($to);
+		$this->email->subject($subject);
+		$this->email->message($message);
 
-			if ($this->email->send()) {
-				redirect('users/check_verification_email');
-			}else {
-				show_error($this->email->print_debugger());
-			}
-		}
-
-		public function check_verification_email(){
-			$data['email'] = $this->session->userdata('email');
-			$this->form_validation->set_rules('verification_code', 'Verification code', 'required');
-
-			if($this->form_validation->run() === FALSE){
-				$this->load->view('templates/header');
-				$this->load->view('users/confirm_email',$data);
-				$this->load->view('templates/footer');
-			}else{
-				$verification_code = intval($this->session->userdata('verification_code'));
-				$posted_code = intval($this->input->post('verification_code'));	
-				if($posted_code == $verification_code){
-					$this->session->unset_userdata('verification_code');
-					$this->session->set_flashdata('valid_verification_code',
-					'Your Verification Code is correct, you can change your Password now!');
-					redirect('users/update_password');
-				}else{
-					$this->session->set_flashdata('invalid_verification_code',
-					'You have typed an invalid Verification Code.');
-					redirect('users/check_verification_email');
-				}	
-			}
-		}
-
-		public function update_password(){
-			$data['title'] = 'Update your Password';
-
-			$this->form_validation->set_rules('password', 'Password', 'required');
-			$this->form_validation->set_rules('retypepass', 'Retype Password', 'required|matches[password]');
-
-			if($this->form_validation->run() === FALSE){
-				$this->load->view('templates/header');
-				$this->load->view('users/update_password',$data);
-				$this->load->view('templates/footer');
-			} else {		
-				$enc_password = md5($this->input->post('password'));
-				$email = $this->session->userdata('email');
-				$this->user_model->update_password($enc_password,$email);
-				$this->session->set_flashdata('password_updated',
-					'You have succesfully updated your Password!');
-				$this->session->unset_userdata('email');
-				redirect(base_url());
-			}	
+		if ($this->email->send()) {
+			redirect('users/check_verification_email');
+		} else {
+			show_error($this->email->print_debugger());
 		}
 	}
+
+	public function check_verification_email()
+	{
+		$data['email'] = $this->session->userdata('email');
+		$this->form_validation->set_rules('verification_code', 'Verification code', 'required');
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->load->view('templates/header');
+			$this->load->view('users/confirm_email', $data);
+			$this->load->view('templates/footer');
+		} else {
+			$verification_code = intval($this->session->userdata('verification_code'));
+			$posted_code = intval($this->input->post('verification_code'));
+			if ($posted_code == $verification_code) {
+				$this->session->unset_userdata('verification_code');
+				$this->session->set_flashdata('valid_verification_code',
+					'Your Verification Code is correct, you can change your Password now!');
+				redirect('users/update_password');
+			} else {
+				$this->session->set_flashdata('invalid_verification_code',
+					'You have typed an invalid Verification Code.');
+				redirect('users/check_verification_email');
+			}
+		}
+	}
+
+	public function update_password()
+	{
+		$data['title'] = 'Update your Password';
+
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		$this->form_validation->set_rules('retypepass', 'Retype Password', 'required|matches[password]');
+
+		if ($this->form_validation->run() === FALSE) {
+			$this->load->view('templates/header');
+			$this->load->view('users/update_password', $data);
+			$this->load->view('templates/footer');
+		} else {
+			$enc_password = md5($this->input->post('password'));
+			$email = $this->session->userdata('email');
+			$this->user_model->update_password($enc_password, $email);
+			$this->session->set_flashdata('password_updated',
+				'You have succesfully updated your Password!');
+			$this->session->unset_userdata('email');
+			redirect(base_url());
+		}
+	}
+}
